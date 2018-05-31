a5871de9c45c6955f17dd556418055db9c401220
bossodiz <bb_boss37@hotmail.com>
2018-05-31 18:06:05 +0700

    update time

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var limitTime = 21600000;
 
 var addtimezone = 25200000;
 
+ 
+var newtimekutum = ['00:00','18:00','02:00','00:00','18:00','02:00','00:00','18:00','02:00','00:00','18:00'];
+var round = 0;
 
 var moment = require('moment-timezone');
 
@@ -17,6 +20,7 @@ var kutumRespawnEnd = new Date();
 
 var bossreset = true;
 
+
 //event นี้ทำงานเมื่อ login สำเร็จ
 botRem.on('ready', () => {
     console.log('Kutum ready!');
@@ -25,41 +29,58 @@ botRem.on('ready', () => {
 botRem.on('message', message => { 
     var command = message.content.replace(/\s\s+/g, ' ');
     if (command === 'คูทุม') {
-        if(!bossreset){
-            message.reply('คูทุมจะเกิดเวลา '+convertTime(kutumRespawnStart)+' น. - ' +  convertTime(kutumRespawnEnd) + ' น.'  );
-        }else{
-            message.reply('เซิฟเวอร์ปิดปรับปรุง รอเวลารายงานใหม่');
-        }
+        message.reply('คูทุมจะเกิดเวลา '+newtimekutum[round]+' น.');
+        // if(!bossreset){
+        //     message.reply('คูทุมจะเกิดเวลา '+convertTime(kutumRespawnStart)+' น. - ' +  convertTime(kutumRespawnEnd) + ' น.'  );
+        // }else{
+        //     message.reply('เซิฟเวอร์ปิดปรับปรุง รอเวลารายงานใหม่');
+        // }
     }else if(command === 'คูทุมรีเซ็ต'){
-        bossreset = true;
-        message.reply('คูทุมรีเซ็ต');
+        // bossreset = true;
+        // message.reply('คูทุมรีเซ็ต');
     }else if(command === 'คูทุมตาย'){
-        bossreset = false;
-        kutumDead = new Date(moment.now()+addtimezone);
-        kutumRespawnStart = new Date(moment.now()+loopStart+addtimezone);
-        kutumRespawnEnd = new Date(moment.now()+loopEnd+addtimezone);
-        message.reply('รีเซ็ตลูปเกิด คูทุมตายเวลา '+convertTime(kutumDead)+ ' น.');
+        message.reply('คูคุมตายรอบเวลา '+newtimekutum[round]+ ' น. / รอบต่อไปเวลา '+newtimekutum[round+1]+' น.');
+        round+=1;
+        if(round === 11){
+            round = 0;
+        }
+        // bossreset = false;
+        // kutumDead = new Date(moment.now()+addtimezone);
+        // kutumRespawnStart = new Date(moment.now()+loopStart+addtimezone);
+        // kutumRespawnEnd = new Date(moment.now()+loopEnd+addtimezone);
+        // message.reply('รีเซ็ตลูปเกิด คูทุมตายเวลา '+convertTime(kutumDead)+ ' น.');
+    }else if(command === 'คูทุมเวลา'){
+        var msg =   '\n'+
+                    'จันทร์\t00:00\t18:00\n'+
+                    'อังคาร\t02:00\n'+
+                    'พุธ\t00:00\t18:00\n'+
+                    'พฤหัส\t02:00\n'+
+                    'ศุกร์\t00:00\t18:00\n'+
+                    'เสาร์\t02:00\n'+
+                    'อาทิตย์\t00:00\t18:00';
+        message.reply(msg);   
+    
     }else if(command.substring(0,9) === 'คูทุมเกิด'){
-        var valuetext = command.substring(10,command.length).split(" ");
-        var a = valuetext[0];
-        var b = valuetext[1];
-        var c = valuetext[2];
+        // var valuetext = command.substring(10,command.length).split(" ");
+        // var a = valuetext[0];
+        // var b = valuetext[1];
+        // var c = valuetext[2];
 
-        if(c === 'ชม'){
-            var time = parseInt(b)*60*60*1000;
-        }else{
-            var time = parseInt(b)*60*1000;
-        }
-        if(a === '+'){
-            kutumRespawnStart = kutumRespawnStart.setTime(kutumRespawnStart.getTime()+time);
-            kutumRespawnEnd = kutumRespawnEnd.setTime(kutumRespawnEnd.getTime()+time);
-        }else{
-            kutumRespawnStart = kutumRespawnStart.setTime(kutumRespawnStart.getTime()-time);
-            kutumRespawnEnd = kutumRespawnEnd.setTime(kutumRespawnEnd.getTime()-time);
-        }
-        kutumRespawnStart = new Date(kutumRespawnStart);
-        kutumRespawnEnd = new Date(kutumRespawnEnd);
-        message.reply('คูทุมจะเกิดเวลา '+convertTime(kutumRespawnStart)+' น. - ' +  convertTime(kutumRespawnEnd) + ' น.'  );
+        // if(c === 'ชม'){
+        //     var time = parseInt(b)*60*60*1000;
+        // }else{
+        //     var time = parseInt(b)*60*1000;
+        // }
+        // if(a === '+'){
+        //     kutumRespawnStart = kutumRespawnStart.setTime(kutumRespawnStart.getTime()+time);
+        //     kutumRespawnEnd = kutumRespawnEnd.setTime(kutumRespawnEnd.getTime()+time);
+        // }else{
+        //     kutumRespawnStart = kutumRespawnStart.setTime(kutumRespawnStart.getTime()-time);
+        //     kutumRespawnEnd = kutumRespawnEnd.setTime(kutumRespawnEnd.getTime()-time);
+        // }
+        // kutumRespawnStart = new Date(kutumRespawnStart);
+        // kutumRespawnEnd = new Date(kutumRespawnEnd);
+        // message.reply('คูทุมจะเกิดเวลา '+convertTime(kutumRespawnStart)+' น. - ' +  convertTime(kutumRespawnEnd) + ' น.'  );
     }
 
 
